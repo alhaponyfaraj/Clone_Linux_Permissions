e032c50c1956b5a9b4f91a1f1c9ed311da8ffdb1
Faraj Alhapony <alhaponyfarag@gmail.com>
2020-11-08 13:05:11 +0200

    adding dirs check

--- clone_permissions.sh
+++ clone_permissions.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
+ 
+################################################################
+##                                                            ##
+##            Clone permissions of a file structure           ##
+##                       Faraj Alhapony                       ##
+##                                                            ##
+################################################################
 
+Dir_Check=(
+  $1
+  $2
+)
+
+for dir in "${Dir_Check[@]}";
+do 
+    if [ ! -d $dir ] 
+    then
+
+        echo "Directory " ${dir} " DOES NOT exists."
+        break 
+        exit 9999 # die with error code 9999
+    fi
+done
