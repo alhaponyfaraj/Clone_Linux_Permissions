b4b6869041ebc7b63a4f3721a8d4b8658ca3ee00
Faraj Alhapony <alhaponyfarag@gmail.com>
2020-11-08 13:06:18 +0200

    Adding dirs stuff

--- clone_permissions.sh
+++ clone_permissions.sh
@@ -22,3 +22,11 @@ do
         exit 9999 # die with error code 9999
     fi
 done
+
+# dirs stuff
+reference_dir=$(find $1 -type d)
+target_dir=$(find $2 -type d)
+
+chown --reference=$reference_dir $target_dir
+chmod --reference=$reference_dir $target_dir
+chgrp --reference=$reference_dir $target_dir
