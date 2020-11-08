de9cdf4efa6c69c0e42ed2fcf86306c7419f25b0
Faraj Alhapony <alhaponyfarag@gmail.com>
2020-11-08 13:06:51 +0200

    Adding files stuff

--- clone_permissions.sh
+++ clone_permissions.sh
@@ -30,3 +30,13 @@ target_dir=$(find $2 -type d)
 chown --reference=$reference_dir $target_dir
 chmod --reference=$reference_dir $target_dir
 chgrp --reference=$reference_dir $target_dir
+
+# files stuff
+reference_file=$(find $1 -type f)
+target_file=$(find $2 -type f)
+
+chown --reference=$reference_file $target_file
+chmod --reference=$reference_dir $target_file
+chgrp --reference=$reference_dir $target_dir
+
+
